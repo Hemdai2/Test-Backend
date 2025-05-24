0dbf8993a4322a63b47c1f88747d2b48d081eec5
hemdai2 <hemanta.adhikari86@gmail.com>
2025-05-24 18:13:11 +0200

    Test order creation success

--- backend/glace/tests/test_api.py
+++ backend/glace/tests/test_api.py
@@ -7,6 +7,9 @@ from ..models import Flavor, Tub
 
 @pytest.mark.django_db
 def test_out_of_stock_flavor():
+    """
+    Test pour errorer si le nombre de boules est insuffisant
+    """
     client = APIClient()
     flavor = FlavorFactory(name="Cherry")
     Tub.objects.create(flavor=flavor, scoops_left=1)
@@ -25,3 +28,19 @@ def test_out_of_stock_flavor():
         "Il ne reste plus assez de boules pour Cherry. Il ne reste que 1."
         in response.json()["error"]
     )
+
+
+@pytest.mark.django_db
+def test_order_creation_success():
+    """Test pour vérifier la création d'un objet Order Success"""
+    client = APIClient()
+    flavor = FlavorFactory(name="Chocolate")
+    TubFactory(flavor=flavor)
+
+    order_data = {"comments": "Test", "scoops": [{"flavor": flavor.id, "quantity": 2}]}
+
+    response = client.post(
+        "/api/glace/create-order/", data=order_data, content_type="application/json"
+    )
+    assert response.status_code == 201
+    assert response.json()["total_price"] == 4
