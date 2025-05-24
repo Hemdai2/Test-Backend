e90057e28133a75ae58c91f1b3fcc54b76d4103d
hemdai2 <hemanta.adhikari86@gmail.com>
2025-05-24 16:00:17 +0200

    Create test for Model

---
+++ backend/glace/tests/test_model.py
@@ -0,0 +1,28 @@
+import pytest
+from glace.models import Tub, Flavor, Order, OrderItem
+
+
+@pytest.mark.django_db
+def test_flavor_object_creation():
+    flavor = Flavor.objects.create(name="Vanilla")
+    assert flavor.name == "Vanilla"
+
+
+@pytest.mark.django_db
+def test_tub_object_creation():
+    flavor = Flavor.objects.create(name="Chocolate")
+    tub = Tub.objects.create(flavor=flavor, scoops_left=40)
+
+    assert tub.flavor == flavor
+    assert tub.scoops_left == 40
+
+
+@pytest.mark.django_db
+def test_create_order():
+    vanilla = Flavor.objects.create(name="Vanilla")
+    chocolate = Flavor.objects.create(name="Chocolate")
+    order = Order.objects.create()
+    OrderItem.objects.create(order=order, flavor=vanilla, quantity=2)
+    OrderItem.objects.create(order=order, flavor=chocolate, quantity=3)
+
+    assert order.total_price() == 10
