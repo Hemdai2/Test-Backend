4a4f8fac400017aa34576064e83c67faac519a79
hemdai2 <hemanta.adhikari86@gmail.com>
2025-05-24 16:24:58 +0200

    Fix test for models

--- backend/glace/tests/test_model.py
+++ backend/glace/tests/test_model.py
@@ -4,12 +4,14 @@ from glace.models import Tub, Flavor, Order, OrderItem
 
 @pytest.mark.django_db
 def test_flavor_object_creation():
+    """Test pour vérifier la création d'un objet Flavor"""
     flavor = Flavor.objects.create(name="Vanilla")
     assert flavor.name == "Vanilla"
 
 
 @pytest.mark.django_db
 def test_tub_object_creation():
+    """Test pour vérifier la création d'un objet Tub"""
     flavor = Flavor.objects.create(name="Chocolate")
     tub = Tub.objects.create(flavor=flavor, scoops_left=40)
 
@@ -19,6 +21,7 @@ def test_tub_object_creation():
 
 @pytest.mark.django_db
 def test_create_order():
+    """Test pour vérifier la création d'un objet Order avec plusieurs OrderItem"""
     vanilla = Flavor.objects.create(name="Vanilla")
     chocolate = Flavor.objects.create(name="Chocolate")
     order = Order.objects.create()
